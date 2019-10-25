3afb9462de7a7d837b9847b7dbed6032a7ab00c8
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 12:27:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,15 @@ def load_library(file)
   # code goes here
   emotes_list = YAML.load_file(file)
   # set up container
-  get_meaning = Hash.new do |
+  emotes = Hash.new do |k,v|
+    get_meaning[v] = {}
+    get_emoticon[v] = {}
+  end
+  emotes_list.each do |meaning, values|
+    emotes[get_meaning] = face[1]
+    emotes[get_emoticon] = face[0]
+  end
+  p emotes
 end
 
 def get_japanese_emoticon
