cd5c7ab64cdacd40b83a063e9c0a6d9bbe7c68c0
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 12:16:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 # require modules here
+require "yaml"
 
-def load_library
+
+def load_library(file)
   # code goes here
+  emotes_list = YAML.load_file(file)
+  # set up container
+  get_meaning = Hash.new do |
 end
 
 def get_japanese_emoticon
