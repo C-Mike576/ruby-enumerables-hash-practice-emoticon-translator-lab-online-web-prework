ca290398d38cea30728aa50f1be521c9847e5ad7
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 12:58:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,10 @@ require "yaml"
 def load_library(file)
   # code goes here
   emotes_list = YAML.load_file(file)
-  emotes = {}
+  emotes = 
   get_meaning = {}
   get_emoticon = {}
-  emotes_list.each_value do |meaning, values|
+  emotes_list.each_value do |values|
     emotes[get_meaning][values[0]] = values[1]
     emotes[get_emoticon][values[1]] = values[0]
   end
