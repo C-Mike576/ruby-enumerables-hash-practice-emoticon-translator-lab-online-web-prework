b1c0d1a29c93c22bf8b4d7d0ea45ed489b060525
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 13:46:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,6 @@ def get_japanese_emoticon(file, emoticon)
   # code goes here
   emoticon_list = load_library(file)
   if emoticon_list["get_emoticon"].has_key?(emoticon)
-    binding.pry
     p emoticon_list["get_emoticon"][emoticon]
   else
     puts "#{emoticon} not found"
